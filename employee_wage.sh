791e953503d39b78d2e30c767ede988a9f67e98d
My name <me@example.com>
2024-01-13 12:55:27 +0530

    Feature added: Monthly wage

--- employee_wage.sh
+++ employee_wage.sh
@@ -11,11 +11,24 @@ wage_per_hour=20
 hourWorked=$((RANDOM%9))
 if [ $isPresent -eq $checkAttendance ]
 then
-echo "Today wages: $((hourWorked*wage_per_hour))"
+todayWages=$((hourWorked*wage_per_hour))
+echo "Hours worked for whole month : $hourWorked"
+echo "Today wages: $todayWages"
 else
 echo "No payment for today"
 fi
 
 
 partTime=8
-echo "Part Time wage: $((partTime*wage_per_hour))"
+partTimeWage=$((partTime*wage_per_hour))
+echo "Part Time wage: $partTimeWage"
+
+
+daysWorked=$((RANDOM%21))
+if [ $isPresent -eq $checkAttendance ]
+then
+echo "Total working  days: $daysWorked"
+monthlyWage=$((daysWorked*hourWorked*wage_per_hour))
+echo "Total wages earned: $monthlyWage"
+fi
+
